199a481b180e1e48976d5585829470a34126452b
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 10:25:27 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 
 export default class Bomb extends Component {
-
+  constructor(props){
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
 
   render() {
+
     return (
-      <div></div>
+      <div>
+    {  this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`}
+      </div>
     )
   }
 }
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 
 
 export default class ImageSlider extends Component {
+  constructor(){
+    super()
+    this.state = {
+    currentSlideIndex: 0
+  }
+  }
 
-
-  render() {
-    return (
-      <div></div>
+    render() {
+      return (
+        <div>
+        I am on slide {this.state.currentSlideIndex}
+        </div>
     )
   }
 }
